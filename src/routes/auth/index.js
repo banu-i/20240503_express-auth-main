09d50bc5faa6b3008933bbba5fc6fe22ffb23792
banu-i <152266442+banu-i@users.noreply.github.com>
2024-05-03 17:49:18 +0200

    Kursaufgabe 20240503
    Aufgabe 1 - komplett

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+// const TodoModel = require("../../database/models/TodoModel");
+const UserModel = require("../../database/models/UserModel");
+const AccessTokens = require("../../services/auth/AccessToken");
 
 const AuthRouter = Router();
 
@@ -7,9 +10,20 @@ const AuthRouter = Router();
 
 AuthRouter.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  // TODO: Login basierend auf email/password
-  // Token soll erstellt werden und zurückgegeben werden
-  res.send("Ich bin nur ein Platzhalter");
+  if (!email || !password) {
+    res.status(StatusCodes.BAD_REQUEST).send(ReasonPhrases.BAD_REQUEST);
+    return;
+  }
+  const user = await UserModel.findOne({ where: { email } });
+
+  if (user.password !== password) {
+    res.status(StatusCodes.UNAUTHORIZED).send(ReasonPhrases.UNAUTHORIZED);
+    return;
+  }
+
+  const myToken = AccessTokens.createAccessToken(user.id);
+
+  res.status(StatusCodes.OK).json({ user, tokens: { accessToken: myToken } });
 });
 
 AuthRouter.post("/signup", async (req, res) => {
@@ -18,9 +32,10 @@ AuthRouter.post("/signup", async (req, res) => {
     res.status(StatusCodes.BAD_REQUEST).send(ReasonPhrases.BAD_REQUEST);
     return;
   }
-  // TODO: Signup basierend auf email, password, name, profileImgUrl
-  // Token soll erstellt werden und zurückgegeben werden
-  res.send("Ich bin nur ein Platzhalter");
+
+  const user = await UserModel.create({ email, password, name, profileImgUrl });
+  const myToken = AccessTokens.createAccessToken(user.id);
+  res.status(StatusCodes.OK).json({ user, tokens: { accessToken: myToken } });
 });
 
 module.exports = { AuthRouter };
