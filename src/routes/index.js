48ff9308822f61a28aa96253e1e182801da8496d
banu-i <152266442+banu-i@users.noreply.github.com>
2024-05-03 17:59:02 +0200

    Korrektur

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,6 @@ const authMiddleWare = require("../middlewares/authMiddleware");
 const AppRouter = Router();
 
 AppRouter.use("/auth", AuthRouter);
-// TODO: Die Users Router soll die auth Middleware durchlaufen
 AppRouter.use("/users", authMiddleWare, UserRouter);
 
 module.exports = { AppRouter };
